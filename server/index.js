3a335eca73abd578c06186cbed756b9bde8807e9
Naru-hub <1208ny0324@gmail.com>
2022-10-03 21:33:41 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 5050;
+// http://localhost:5050/
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.listen(PORT, () => {
+  console.log("ローカルサーバー起動中・・・");
+});
