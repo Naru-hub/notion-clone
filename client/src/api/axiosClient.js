e6149151a099fade2e45be4983af61b0956bd783
Naru-hub <1208ny0324@gmail.com>
2022-10-10 22:10:24 +0900

    Axios interceptorsを利用してリクエストの前処理を設定

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5050/api/v1";
+const getToken = () => localStorage.getItem("token");
+
 const axiosClient = axios.create({
   baseURL: BASE_URL,
+});
+
+// APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async (config) => {
+  return {
+    config,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getToken()}`, // リクエストヘッダにJWTをつけてサーバーに渡す
+    },
+  };
 });
