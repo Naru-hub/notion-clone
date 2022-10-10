aa0b2d7ee9232a1780489953945e3937ca16f3f4
Naru-hub <1208ny0324@gmail.com>
2022-10-10 22:14:46 +0900

    Axios interceptorsを利用してレスポンスの前処理を設定

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -17,3 +17,14 @@ axiosClient.interceptors.request.use(async (config) => {
     },
   };
 });
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err) => {
+    throw err.response;
+  }
+);
+
+export default axiosClient;
