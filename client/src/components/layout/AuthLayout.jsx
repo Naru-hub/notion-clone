f35c26eb3f60fa62bf720725ab8aafa0353cf67d
Naru-hub <1208ny0324@gmail.com>
2022-10-13 22:29:54 +0900

    JWTチェックでtokenの有無に応じてリダイレクトされる実装

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
 
 const AuthLayout = () => {
   const navigate = useNavigate();
@@ -11,9 +12,14 @@ const AuthLayout = () => {
     // JWTを持っているのか確認する
     const checkAuth = async () => {
       // 認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if (isAuth) {
+        navigate("/");
+      }
     };
     checkAuth();
   }, [navigate]);
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
