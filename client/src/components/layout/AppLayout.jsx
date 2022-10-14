51e6c7ea576fca6fc89f02b85a71b1a3fbf72b28
Naru-hub <1208ny0324@gmail.com>
2022-10-14 22:20:42 +0900

    AppLayoutでNotionアプリ共通コンポーネントを作成

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Container } from "@mui/system";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Box } from "@mui/material";
+import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
+import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // JWTを持っているのか確認する
+    const checkAuth = async () => {
+      // 認証チェック
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default AppLayout;
