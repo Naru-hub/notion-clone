f7ce41613b89092267656b7034ffd9057327816b
Naru-hub <1208ny0324@gmail.com>
2022-10-17 21:35:12 +0900

    memo.jsの綴りミスを訂正

--- server/src/v1/controllers/memo.js
+++ server/src/v1/controllers/memo.js
@@ -6,7 +6,7 @@ exports.create = async (req, res) => {
     // メモ新規作成
     const memo = await Memo.create({
       user: req.user._id,
-      position: memoCounte > 0 ? memoCount : 0,
+      position: memoCount > 0 ? memoCount : 0,
     });
     res.status(201).json(memo);
   } catch {
