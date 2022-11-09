9d534fb416557718e73919dd29ec83e1244cff67
Naru-hub <1208ny0324@gmail.com>
2022-11-09 20:59:16 +0900

    メモのタイトルと詳細を出力する

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/system";
 import { IconButton, TextField } from "@mui/material";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
@@ -8,12 +8,15 @@ import memoApi from "../api/memoApi";
 
 const Memo = () => {
   const { memoId } = useParams();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     const getMemo = async () => {
       try {
         const res = await memoApi.getOne(memoId);
-        console.log(res);
+        setTitle(res.title);
+        setDescription(res.description);
       } catch (err) {
         alert(err);
       }
@@ -39,6 +42,7 @@ const Memo = () => {
       </Box>
       <Box sx={{ padding: "10px 50px" }}>
         <TextField
+          value={title}
           placeholder="無題"
           variant="outlined"
           fullWidth
@@ -49,6 +53,7 @@ const Memo = () => {
           }}
         />
         <TextField
+          value={description}
           placeholder="追加"
           variant="outlined"
           fullWidth
