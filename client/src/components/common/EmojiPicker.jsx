afa6bf5b93322a05a2f0da486d834b8e53b059cb
Naru-hub <1208ny0324@gmail.com>
2022-11-17 22:07:14 +0900

    絵文字アイコンの大きさなどを修正

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function EmojiPicker(props) {
-  return <div>{props.icon}</div>;
+  const [selectedEmoji, setSelectedEmoji] = useState();
+
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+
+  return (
+    <Box>
+      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+        {selectedEmoji}
+      </Typography>
+    </Box>
+  );
 }
 
 export default EmojiPicker;
